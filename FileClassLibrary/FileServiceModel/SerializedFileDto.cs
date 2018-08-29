1bba3318d04f16b9088b664b91b6303939f3c535
danIancuV <33359205+danIancuV@users.noreply.github.com>
2018-08-29 12:52:11 +0300

    model added

---
+++ FileClassLibrary/FileServiceModel/SerializedFileDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileClassLibrary.FileServiceModel
+{
+    [Serializable]
+    public class SerializedFileDto
+    {
+        public string Name { get; set; }
+
+        public string FileContent { get; set; }
+
+        public string Extension { get; set; }
+    }
+}
