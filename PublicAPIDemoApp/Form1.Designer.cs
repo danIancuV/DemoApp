a8776e524b34b84d35998a78f8a64a6c6e592644
danIancuV <33359205+danIancuV@users.noreply.github.com>
2018-10-06 20:17:22 +0300

    currency converter added

--- PublicAPIDemoApp/Form1.Designer.cs
+++ PublicAPIDemoApp/Form1.Designer.cs
@@ -34,17 +34,32 @@
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.cADDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.eURDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.uSDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ratesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ratesBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Location = new System.Drawing.Point(7, 19);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 23);
             this.button1.TabIndex = 0;
@@ -54,6 +69,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -61,17 +77,17 @@
             this.eURDataGridViewTextBoxColumn,
             this.uSDDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.ratesBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 42);
+            this.dataGridView1.Location = new System.Drawing.Point(7, 48);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(344, 64);
+            this.dataGridView1.Size = new System.Drawing.Size(345, 45);
             this.dataGridView1.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(154, 160);
+            this.label1.Location = new System.Drawing.Point(165, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 6;
             this.label1.Text = "currency_rate";
             // 
@@ -82,9 +98,9 @@
             "CAD",
             "EUR",
             "USD"});
-            this.comboBox1.Location = new System.Drawing.Point(12, 157);
+            this.comboBox1.Location = new System.Drawing.Point(6, 44);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(123, 21);
+            this.comboBox1.Size = new System.Drawing.Size(136, 21);
             this.comboBox1.TabIndex = 7;
             this.comboBox1.Text = "---------------------";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
@@ -92,11 +108,134 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 141);
+            this.label2.Location = new System.Drawing.Point(6, 28);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.Size = new System.Drawing.Size(147, 13);
             this.label2.TabIndex = 8;
             this.label2.Text = "Please select a currency";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(151, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(6, 39);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Currency I have";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(253, 39);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(254, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Currency I want";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Initial Amount";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(262, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(257, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Converted Amount";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(429, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(282, 78);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Today exchange rate";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(20, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(362, 100);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Main currencies exchange rates";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox2);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.comboBox3);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox3.Location = new System.Drawing.Point(20, 176);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(385, 146);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Currency converter";
             // 
             // cADDataGridViewTextBoxColumn
             // 
@@ -124,18 +263,20 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(740, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ratesBindingSource)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -150,6 +291,18 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- PublicAPIDemoApp/Form1.cs
+++ PublicAPIDemoApp/Form1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -12,6 +14,10 @@ namespace PublicAPIDemoApp
         public Form1()
         {
             InitializeComponent();
+            GetRatesList();
+            comboBox2.DataSource = GetRatesList();
+            comboBox3.DataSource = GetRatesList();
+            textBox2.ReadOnly = true;
         }
 
         private void button1_ClickAsync(object sender, EventArgs e)
@@ -20,25 +26,35 @@ namespace PublicAPIDemoApp
             dataGridView1.DataSource = currencyResult;
         }
 
-        public static List<Rates> GetCurrency()
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetSelectedCurrency();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetConvertedFinalAmount();
+        }
+
+        private static List<Rates> GetCurrency()
         {
             const string apiUrl = "http://data.fixer.io/api/latest?access_key=";
             const string apiKey = "c6469effe16603f8a5b21335e6b9b027";
 
             using (var client = new HttpClient())
             {
-                const string repUrl = apiUrl + apiKey + "&symbols=CAD,EUR,USD&format=1";
+                const string repUrl = apiUrl + apiKey + "&format=1";
                 var response = client.GetAsync(repUrl).Result;
 
                 if (!response.IsSuccessStatusCode) return null;
                 var result = response.Content.ReadAsStringAsync().Result;
                 var rootResult = JsonConvert.DeserializeObject<RootObject>(result);
                 var ratesList = new List<Rates> { rootResult.rates };
-                return ratesList;
+                return ratesList;               
             }
         }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+ 
+        private void GetSelectedCurrency()
         {
             var currencyResultList = GetCurrency();
             var selectedCadCurrencyResult = currencyResultList[0].CAD;
@@ -49,17 +65,52 @@ namespace PublicAPIDemoApp
             switch (currency)
             {
                 case "CAD":
-                    label1.Text = selectedCadCurrencyResult.ToString();
+                    label1.Text = selectedCadCurrencyResult.ToString(CultureInfo.CurrentCulture);
                     break;
                 case "EUR":
-                    label1.Text = selectedEurCurrencyResult.ToString();
+                    label1.Text = selectedEurCurrencyResult.ToString(CultureInfo.CurrentCulture);
                     break;
                 case "USD":
-                    label1.Text = selectedUsdCurrencyResult.ToString();
+                    label1.Text = selectedUsdCurrencyResult.ToString(CultureInfo.CurrentCulture);
                     break;
+            }
+        } 
+
+        private void GetConvertedFinalAmount()
+        {
+            var rates = GetCurrency()[0];
+            var fromCurrencyName = comboBox2.SelectedItem.ToString();
+            var toCurrencyName = comboBox3.SelectedItem.ToString();
+            var selectedFromCurrencyRateValue =
+                Convert.ToDouble(typeof(Rates).GetProperty(fromCurrencyName)?.GetValue(rates));
+            var selectedToCurrencyRateValue =
+                Convert.ToDouble(typeof(Rates).GetProperty(toCurrencyName)?.GetValue(rates));           
+            try
+            {
+                var initialAmount = Double.Parse(textBox1.Text);
+                var finalAmount = Math.Round(((initialAmount / selectedFromCurrencyRateValue) * selectedToCurrencyRateValue), 2);
+                
+                textBox2.Text = finalAmount.ToString(CultureInfo.CurrentCulture);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(@"Please specify an amount!");
             }
         }
 
+        private List<string> GetRatesList()
+        {
+            Rates rates = GetCurrency()[0];
+            var properties = rates.GetType().GetProperties().ToList();
+            var fields = new List<string>();
+            foreach (var prop in properties)
+            {
+                fields.Add(prop.Name);
+            }
+
+            return fields;
+        }
 
     }
+
 }
